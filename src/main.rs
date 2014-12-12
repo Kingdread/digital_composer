ef51c73787adc2e395b5a96ba55b7d53ec7b6c2e
Daniel <kingdread@gmx.de>
2014-12-12 19:39:52 +0100

    Switched to docopt
    * Nicer code
    * Nicer help
    * 10/10 would switch again
    * 12/10 with rice

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![feature(macro_rules)]
-extern crate getopts;
-use getopts::{optopt, optflag, getopts, OptGroup};
-use getopts::HasArg::{Yes, No, Maybe};
-use getopts::Occur::{Req, Optional, Multi};
+#![feature(phase)]
+extern crate serialize;
+extern crate docopt;
+#[phase(plugin)] extern crate docopt_macros;
 use markov::MarkovChain;
 use std::io;
-use std::os;
 use std::error::Error;
 use std::hash::hash;
 mod markov;
@@ -37,40 +36,6 @@ fn print_errorstack(err: &Error) {
     }
 }
 
-fn format_option(opt: &OptGroup) -> String {
-    //! Format an OptGoup to print in the help, e.g.
-    //!     -o FILE, --output=FILE Sets the output filename
-    format!("    -{}{}, --{}{}    {} [{}]",
-            opt.short_name,
-            match opt.hasarg {
-                Yes => format!(" {}", opt.hint),
-                No => "".to_string(),
-                Maybe => format!(" [{}]", opt.hint),
-            },
-            opt.long_name,
-            match opt.hasarg {
-                Yes => format!("={}", opt.hint),
-                No => "".to_string(),
-                Maybe => format!("[={}]", opt.hint),
-            },
-            opt.desc,
-            match opt.occur {
-                Req => "Required",
-                Optional => "Optional",
-                Multi => "Multi",
-            },
-            )
-}
-
-fn print_usage(options: &[OptGroup]) {
-    //! Print the usage help and every possible argument, given by
-    //! options.
-    println!("Usage: {} ARGS input-file trackno", os::args()[0]);
-    for opt in options.iter() {
-        println!("{}", format_option(opt));
-    }
-}
-
 fn compose(notes: &Vec<u8>, degree: uint, length: uint) -> Vec<u8> {
     //! Takes an original sequence of notes and creates a new composition
     let mut m = MarkovChain::<u64, u8>::new();
@@ -96,59 +61,33 @@ fn compose(notes: &Vec<u8>, degree: uint, length: uint) -> Vec<u8> {
     };
     composition
 }
-    
-macro_rules! opt_argument(
-    ($matches:expr, $name:expr, $ty:ty, $default:expr, $opts:expr) => (
-        match $matches.opt_str($name) {
-            Some(arg) => match from_str::<$ty>(arg.as_slice()) {
-                Some(conv) => conv,
-                None => {
-                    print_usage($opts);
-                    return;
-                },
-            },
-            None => $default,
-        };
-    );
+
+
+docopt!(Args deriving Show, "
+Usage:
+    digital_composer [options] <input> <track>
+    digital_composer --help
+
+Options:
+    -h, --help                  Show this help message
+    -o <file>, --output <file>  Specify the output file name [default: composition.mid]
+    -l <len>, --length <len>    Specify the length in notes [default: 100]
+    -d <deg>, --degree <deg>    Specify the degree of the markov chain [default: 1]
+",
+    arg_track: uint,
+    flag_length: uint,
+    flag_degree: uint,
 )
 
 fn main() {
-    let opts = &[
-        optopt("o", "output", "The output file name, defaults to composition.mid", "FILE"),
-        optopt("l", "length", "Length of the resulting composition, defaults to 100", "LENGTH"),
-        optopt("d", "degree", "Degree of the markov chain, defaults to 1", "DEG"),
-        optflag("h", "help", "Show the help"),
-    ];
     // Argument parsing and validating
-    let matches = match getopts(os::args().tail(), opts) {
-        Ok(m) => m,
-        Err(err) => {
-            let _ = write!(&mut io::stderr(), "{}\n", err);
-            return;
-        }
-    };
-    if matches.opt_present("h") {
-        print_usage(opts);
-        return;
-    }
-    if matches.free.len() != 2 {
-        print_usage(opts);
-        return;
-    }
-    let input_filename = matches.free[0].as_slice();
-    let output_filename = match matches.opt_str("o") {
-        Some(f) => f,
-        None => "composition.mid".to_string(),
-    };
-    let input_trackno = match from_str::<uint>(matches.free[1].as_slice()) {
-        Some(n) => n,
-        None => {
-            print_usage(opts);
-            return;
-        },
-    };
-    let degree = opt_argument!(matches, "d", uint, 1, opts);
-    let length = opt_argument!(matches, "l", uint, 100, opts);
+    // Much shorter thanks to docopt
+    let args: Args = Args::docopt().decode().unwrap_or_else(|err| err.exit());
+    let input_filename = args.arg_input;
+    let output_filename = args.flag_output;
+    let input_trackno = args.arg_track;
+    let degree = args.flag_degree;
+    let length = args.flag_length;
 
     // Actual program
     println!("Reading {}...", input_filename);
